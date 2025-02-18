0ec3d26c85098227afb2e2caf154290336918b50
dane206 <dane@rs2media.com>
2025-02-17 16:57:01 -0800

    Create Generates Unique IDs.js

---
+++ Generates Unique IDs.js
@@ -0,0 +1,6 @@
+function generateUUID() {
+    return crypto.randomUUID ? crypto.randomUUID() : 'xxxx-xxxx-4xxx-yxxx-xxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
